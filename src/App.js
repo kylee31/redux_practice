e1ca01cd8cfec0cb37b06c14d50090d578fe9490
lky <skyvip2633@gmail.com>
2022-10-13 17:51:00 +0900

    practice redux-thunk

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
 import TodosContainer from "./containers/TodosContainer";
 function App() {
   return (
     <div>
-      <CounterContainer />
-      <hr />
-      <TodosContainer />
+      <SampleContainer />
     </div>
   );
 }
