2bc8392c0f181603d59a0fc19ed3e7a10f6d8086
lky <skyvip2633@gmail.com>
2022-10-25 23:15:22 +0900

    thunk 최적화

--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -12,8 +12,15 @@ const SampleContainer = ({
     loadingUsers
 }) => {
     useEffect(() => {
-        getPost(1);
-        getUsers(1);
+        const fn = async () => {
+            try {
+                await getPost(1);
+                await getUsers(1);
+            } catch (e) {
+                console.log(e);
+            }
+        };
+        fn();
     }, [getPost, getUsers]);
     return (
         <Sample
@@ -26,11 +33,11 @@ const SampleContainer = ({
 };
 
 export default connect(
-    ({ sample }) => ({
+    ({ sample, loading }) => ({
         post: sample.post,
         users: sample.users,
-        loadingPost: sample.loading.GET_POST,
-        loadingUsers: sample.loading.GET_USERS
+        loadingPost: loading['sample/GET_POST'],
+        loadingUsers: loading['sample/GET_USERS']
     }),
     {
         getPost,
